a36bcdba536fc78e7c9566aea5b83bd88c5926a5
KARTHIKEYAN EKAMBARAM <karthike>
2025-05-29 08:00:40 +0000

    ver 1.0

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
-], (Controller, JSONModel, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+], (Controller, JSONModel, MessageToast, Fragment) => {
     "use strict";
 
     return Controller.extend("mrpscan.mrpscanning.controller.View1", {
         onInit() {
             this.getView().setModel(new JSONModel(), "delvDetails");
+            this.getView().setModel(new JSONModel(), "mrpscanned");
         },
 
         onGoPress() {
@@ -24,6 +26,43 @@ sap.ui.define([
                 },
                 error: () => {
                     MessageToast.show("Failed to retrieve delivery details.");
+                }
+            });
+        },
+
+        onMRPLabelInput() {
+            const sMRPLabelValue = this.getView().byId("inpmrplabel").getValue();
+            const sUrl = `http://wd01.dxcpaas4sap.zf-world.com:8004/am_deliv_serv/mrplabel?mrplabel=${sMRPLabelValue}`;
+
+            jQuery.ajax({
+                url: sUrl,
+                method: "GET",
+                success: (oData) => {
+                    const oMRPScannedModel = this.getView().getModel("mrpscanned");
+                    oMRPScannedModel.setData(oData);
+
+                    const oDelvDetailsModel = this.getView().getModel("delvDetails");
+                    const aDelvData = oDelvDetailsModel.getData();
+                    const aMRPScannedData = oMRPScannedModel.getData();
+
+                    aDelvData.forEach((oDelvItem) => {
+                        const oMatchedMRPItem = aMRPScannedData.find(oMRPItem => oMRPItem.MATNR === oDelvItem.MATNR);
+                        if (oMatchedMRPItem) {
+                            oDelvItem.DIFF_QTY = oDelvItem.LFIMG;
+
+                            if (oDelvItem.DIFF_QTY > oMatchedMRPItem.quantity) {
+                                oDelvItem.SCAN_QTY += oMatchedMRPItem.quantity;
+                                oDelvItem.DIFF_QTY -= oMatchedMRPItem.quantity;
+                            } else {
+                                MessageToast.show("MRP Sticker Qty is greater than delivery Qty");
+                            }
+                        }
+                    });
+
+                    oDelvDetailsModel.setData(aDelvData);
+                },
+                error: () => {
+                    MessageToast.show("Failed to retrieve MRP label details.");
                 }
             });
         }
